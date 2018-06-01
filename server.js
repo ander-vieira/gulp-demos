a35784ae6d9f00c158360eb2d4c7b9eedcb62233
Ander Vieira <ander@ideable.net>
2018-06-01 10:50:38 +0200

    Add base page with links to the demos

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ demos.forEach(function(demo) {
   app.use('/'+demo, express.static(__dirname+'/'+demo+'/public'));
 });
 
+app.get('/', function(req, res) {
+  var html = '<html><body>';
+  demos.forEach(function(demo) {
+    html += '<p><a href="/'+demo+'">'+demo+'</a></p>';
+  });
+  html += '</body></html>';
+  res.send(html);
+});
+
 app.listen(3000, function() {
   console.log('Server listening on port 3000');
 })
