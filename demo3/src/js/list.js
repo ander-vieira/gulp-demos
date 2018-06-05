098644673523631268943cbabcf6b7d6766c08f7
Ander Vieira <ander@ideable.net>
2018-06-05 12:29:19 +0200

    Added saving as JSON to list

--- demo3/src/js/list.js
+++ demo3/src/js/list.js
@@ -1,4 +1,6 @@
-document.querySelectorAll('.list-buttons a').forEach(function(el) {
+window.list = [];
+
+document.querySelectorAll('.list-buttons a:not(.saveButton)').forEach(function(el) {
   el.addEventListener('click', function(event) {
     var list = document.querySelector('.list');
 
@@ -6,6 +8,23 @@ document.querySelectorAll('.list-buttons a').forEach(function(el) {
     li.innerText = this.innerText;
     list.appendChild(li);
 
+    window.list.push(this.innerText);
+
     event.preventDefault();
   });
 });
+
+document.querySelector('.list-buttons a.saveButton').addEventListener('click', function(event) {
+  saveList();
+
+  event.preventDefault();
+});
+
+function saveList() {
+  var url = URL.createObjectURL(new Blob([JSON.stringify(window.list)], {type: 'application/json'}));
+  var a = document.createElement('a');
+  a.href = url;
+  a.download = 'list.json';
+
+  a.click();
+}
